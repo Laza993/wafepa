b30253db294ff8030382983b8a00ba1d293e31a0
Laza993 <lazarevicclaza@yahoo.com>
2020-03-01 23:46:30 +0100

    AngularFrontForActivity

--- src/main/resources/static/app/js/main.js
+++ src/main/resources/static/app/js/main.js
@@ -1,5 +1,119 @@
-var wafepaApp = angular.module("firstApp", []);
+var wafepaApp = angular.module("wafepaApp", ["ngRoute"]);
 
-wafepaApp.controller("firstController", function($scope) {
-	$scope.tekst = "first text";
+
+wafepaApp.controller("ActivityCtrl", function($scope, $http, $location){
+	var url = "api/activities";
+
+	$scope.activities = [];
+
+	var getActivities = function(){
+		$http.get(url).then(
+			function success(res){
+				//console.log(res)
+				$scope.activities = res.data;
+			},
+			function error(){
+				alert("failed to fetch all activities")
+			}
+		)
+	}
+	getActivities();
+
+	$scope.doDeleteActivity = function(id){
+		var url1 = "api/activities/" + id;
+		$http.delete(url1).then(
+			function success(){
+				getActivities();
+			},
+			function error(){
+				alert("deletition failed")
+			}
+		)
+	}
+
+
+	$scope.toEditActivity = function(id){
+		console.log(id)
+		$location.path("/activities/edit/" + id)
+	}
+
+	$scope.toAddActivity = function(){
+		$location.path('/activities/add');
+	}
+
 });
+
+wafepaApp.controller("EditActivityCtrl", function($scope, $http, $location, $routeParams){
+	console.log($routeParams.id);
+
+	var url = "api/activities/" + $routeParams.id;
+
+	$scope.Activity = {};
+	$scope.Activity.name = "";
+
+	var FindActivity = function(){
+		$http.get(url).then(
+			function success(res){
+				console.log(res);
+				$scope.Activity = res.data;
+			},
+			function error(){
+				alert("couldn't fetch Activity");
+			}
+		)
+	}
+	FindActivity();
+
+	$scope.doEdit = function(){
+		$http.put(url, $scope.Activity).then(
+			function success(){
+				$location.path("/activities")
+			},
+			function error(){
+				alert("Unsuccessful edit")
+			}
+		)
+	}
+
+	
+});
+
+wafepaApp.controller("AddActivityCtrl", function($scope, $http, $location){
+	var url = "api/activities/";
+
+	$scope.Activity = {};
+	$scope.Activity.name = "";
+
+	$scope.createActivity = function(){
+		$http.post(url, $scope.Activity).then(
+			function success(){
+				console.log($scope.Activity)
+				$location.path('/activities')
+			},
+			function error(){
+				alert("Creation failed")
+			}
+		)
+	}
+
+});
+
+
+wafepaApp.config(['$routeProvider', function($routeProvider) {
+	$routeProvider
+		.when('/', {
+			templateUrl : '/app/html/home.html'
+		})
+		.when('/activities', {
+			templateUrl : '/app/html/activities.html'
+		})
+		.when('/activities/add', {
+			templateUrl : '/app/html/addActivity.html'
+		})
+		.when('/activities/edit/:id', {
+			templateUrl : '/app/html/edit-activity.html'
+		})
+		.otherwise({
+			redirectTo: '/'
+		});
+}]);

43087c6ad032d0a3f598b7c512e3f964c9dc3b6c
Laza993 <lazarevicclaza@yahoo.com>
2020-04-04 23:59:49 +0200

    countries update

--- target/classes/static/app/js/main.js
+++ target/classes/static/app/js/main.js
@@ -38,28 +38,77 @@ wafepaApp.config(['$routeProvider', function($routeProvider) {
 }]);
 
 wafepaApp.controller("countriesCtrl", function($scope, $http){
-	var url = "https://restcountries.eu/rest/v2/all";
+	var urlAll = "https://restcountries.eu/rest/v2/all";
 
-	$scope.Countries = [];
+	var Countries = [];
 	$scope.page = [];
+	$scope.pageNum = 0;
+	$scope.pageSize = 10;
+	$scope.totalPages = 0;
+	$scope.countries = {};
+	$scope.countries.region = "";
+	$scope.countries.regBlock = "";
 
-	var getCountry = function(){
+	var getCountries = function(){
+		var url = urlAll;
+		if($scope.countries.region != ""){
+			url = "https://restcountries.eu/rest/v2/region/" + $scope.countries.region;
+			console.log(url);
+		}
+		if($scope.countries.regBlock != ""){
+			url = "https://restcountries.eu/rest/v2/regionalbloc/" + $scope.countries.regBlock;
+			console.log(url);
+		}
 		$http.get(url).then(
 			function success(res){
-				$scope.Countries = res.data;
-				console.log($scope.Countries);
-				console.log(res.headers());
-				$scope.page = $scope.Countries.slice(0, 5);
-				console.log($scope.page);
-							
+				Countries = res.data;	
+				var remainder = Countries.length % 10;
+				if( remainder != 0){
+					$scope.totalPages = (Countries.length - remainder) / 10 + 1;
+				}
+				if( remainder == 0){
+					$scope.totalPages = Countries.length / 10;
+				}
+				console.log($scope.totalPages);
+				
+				$scope.pageInitIndex = $scope.pageNum * $scope.pageSize;
+				$scope.pageEndIndex = $scope.pageNum * $scope.pageSize + $scope.pageSize;
+		
+				$scope.page = Countries.slice($scope.pageInitIndex, $scope.pageEndIndex);				
 			},
 			function error(){
 				alert("couldn't find countries");
 			}
 		)
 	}
-	getCountry();
+	getCountries();
 
+	$scope.search = function(){
+		$scope.pageNum = 0;
+		getCountries();
+	}
+
+	$scope.findAll = function(){
+		$scope.countries.region = "";
+		$scope.countries.regBlock = "";
+		$scope.pageNum = 0;
+		getCountries();
+	}
+
+	$scope.goToPage = function(pageNum){
+		$scope.pageNum = pageNum;
+		getCountries();
+	}
+
+	$scope.changePage = function(direction){
+		console.log(direction);
+		
+		$scope.pageNum += direction;
+		console.log($scope.pageNum);
+		
+		getCountries();
+	}
+	
 
 // 	(function() {
 //     'use strict';
